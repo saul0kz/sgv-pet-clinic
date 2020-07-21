feb0f8f3d1e6cb14a6850ca1db3bac17f0df3357
saulo <saul0kz@gmail.com>
2020-07-21 14:55:38 -0300

    first commit

---
+++ src/test/java/saulo/learning/sgvpetclinic/SgvpetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package saulo.learning.sgvpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgvpetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
