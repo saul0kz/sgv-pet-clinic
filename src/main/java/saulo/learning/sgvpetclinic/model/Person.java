ba48c9a6e0463becb4fbd10dcc9a6670f10dd89b
saulo <saulo@getinfo.net.br>
2020-10-26 19:46:39 -0300

    Person -gets and setters

--- src/main/java/saulo/learning/sgvpetclinic/model/Person.java
+++ src/main/java/saulo/learning/sgvpetclinic/model/Person.java
@@ -2,5 +2,21 @@ package saulo.learning.sgvpetclinic.model;
 
 public class Person {
     private String firstName;
-    private String lastName; 
+    private String lastName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
